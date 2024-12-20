cfc9e84e93130aba3f0fc53de1c724e624d439f1
Jagannath <jagannathmahato.dev@gmail.com>
2024-12-20 23:37:57 +0530

    toggle problem solve

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 
-  const menuIcon = document.querySelector('.menu-icon');
-  const nav = document.querySelector('nav');
+  // const menuIcon = document.querySelector('.menu-icon');
+  // const nav = document.querySelector('nav');
 
-  menuIcon.addEventListener('click', () => {
-    menuIcon.classList.toggle('active');
-    nav.classList.toggle('active');
-  });
+  // menuIcon.addEventListener('click', () => {
+  //   menuIcon.classList.toggle('active');
+  //   nav.classList.toggle('active');
+  // });
 
   document.querySelector('.menu-icon').addEventListener('click', function() {
     this.classList.toggle('active');
     document.querySelector('nav').classList.toggle('active');
+    console.log("successfull")
 });
 
